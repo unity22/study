0e92078426f326114020d2bda1cf5e4caf71933b
unity22 <yunaotani22@gmail.com>
2022-04-23 22:44:51 +1200

    init commit for ans1 in study005

---
+++ study_005/answer1.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    line1 = a + b
+    line2 = a - b
+    line3 = a * b
+    print(line1)
+    print(line2)
+    print(line3)
+    
