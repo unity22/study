b8933457e0be9ac8ff64b10206633001e2a7551a
unity22 <yunaotani22@gmail.com>
2022-04-24 21:21:23 +1200

    initial commit for study006

---
+++ study_006/answer.py
@@ -0,0 +1,7 @@
+#For all non-negative integers $i < n$, print $i^{2}$.
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0, n):
+        output = i**2
+        print(output)
+        
