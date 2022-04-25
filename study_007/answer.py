5073269bd656c0296ce75e3359830afcd3621ef0
unity22 <yunaotani22@gmail.com>
2022-04-25 22:11:23 +1200

    fix logic for study007

--- study_007/answer.py
+++ study_007/answer.py
@@ -5,9 +5,9 @@ def is_leap(year):
     leap = False
     if year % 4 == 0:
         leap = True
-    elif year % 100 == 0:
+    if year % 100 == 0:
         leap = False
-    elif year % 400 == 0:
+    if year % 400 == 0:
         leap = True
     return leap
 
