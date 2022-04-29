2d62a8160f25792c51254efe526feccab9b76904
unity22 <yunaotani22@gmail.com>
2022-04-29 20:02:49 +1200

    initial commit for study011

--- study_011/answer.py
+++ study_011/answer.py
@@ -1,2 +1,21 @@
 if __name__ == '__main__':
     N = int(input())
+    array = []
+    for _ in range(0,N):
+        i = input().split()
+        if i[0] == "insert":
+            array.insert(int(i[1]),int(i[2]))
+        if i[0] == "print":
+            print(array)
+        if i[0] == "remove":
+            array.remove(int(i[1]))
+        if i[0] == "append":
+            array.append(int(i[1]))
+        if i[0] == "sort":
+            array.sort()
+        if i[0] == "pop":
+            array.pop()
+        if i[0] == "reverse":
+            array.reverse()
+
+        
